4f3f8932ee5ea39264452e1bbe7b46592d6ede78
Brsantos197 <brsantos197@gmail.com>
2021-03-18 11:07:19 -0300

    chore: Altera retorno da rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('teste', (req, res) => {
-    return res.json({ hello: 'Hello world' })
+    return res.json({ hello: 'Hello world!!!' })
 })
 
 app.listen(3333)
